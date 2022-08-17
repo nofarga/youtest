70418cf2a3050c302aafd54f550e5bbbd17a2684
nofarg <nofarg@pango.co.il>
2022-08-17 17:14:22 +0300

    fix 5 commit

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ from sklearn.metrics import accuracy_score
 import numpy
 import datetime
 import os
-import Orange
-
 
 app = Flask(__name__,template_folder='./templates',static_folder='./static')
 
